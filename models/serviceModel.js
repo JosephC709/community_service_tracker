0dbb30559ee8f2dcbb99d33f48807096be4da433
JosephC709 <josephc709@nycstudents.net>
2026-05-26 00:37:51 -0400

    Update serviceModel.js

--- models/serviceModel.js
+++ models/serviceModel.js
@@ -15,6 +15,9 @@
 const db = require('../db');
 
 // COMMENT FOR getAllRecords:
+// This function gets all community service records from the database.
+// It does not take any parameters.
+// It returns an array of records sorted by the activity date in descending order.
 const getAllRecords = async () => {
   const res = await db.query(
     'SELECT * FROM service_records ORDER BY activity_date DESC'
@@ -23,6 +26,10 @@ const getAllRecords = async () => {
 };
 
 // COMMENT FOR addRecord:
+// This function adds a new community service record to the database.
+// It takes the student's name, student ID, activity date, hours completed,
+// and recipient organization as parameters.
+// It returns the newly created record.
 const addRecord = async (student_name, student_id, activity_date, hours, recipient) => {
   const res = await db.query(
     `INSERT INTO service_records
@@ -35,6 +42,9 @@ const addRecord = async (student_name, student_id, activity_date, hours, recipie
 };
 
 // COMMENT FOR getHoursByStudent:
+// This function calculates the total community service hours for each student.
+// It does not take any parameters.
+// It returns a list of students with their student IDs and total hours completed.
 const getHoursByStudent = async () => {
   const res = await db.query(
     `SELECT student_name, student_id, SUM(hours) AS total_hours
